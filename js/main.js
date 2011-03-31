fe7662a8dbc58e24ed754fd6672a5388e19675b8
bsspirit <bsspirit@163.com>
2011-03-31 17:42:29 +0800

    layout

---
+++ js/main.js
@@ -0,0 +1,5 @@
+//var STATIC_PATH = 'http://1.dotabook.sinaapp.com/static/';
+//var SERVER_PATH = 'http://dotabook.info/'
+
+var STATIC_PATH = 'http://localhost/static/';
+var SERVER_PATH = 'http://localhost:5000/'
